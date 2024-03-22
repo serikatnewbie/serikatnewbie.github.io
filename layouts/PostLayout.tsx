a25f8300a1e71f69bf805c99f9ee63ca30363ea3
FlaBBB <f12345ff67@gmail.com>
2024-03-23 06:28:49 +0700

    fix: overflowed description

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -78,7 +78,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                           className="w-10 h-10 rounded-full"
                         />
                       )}
-                      <dl className="text-sm font-medium leading-5 whitespace-nowrap">
+                      <dl className="text-sm font-medium leading-5 whitespace-normal">
                         <dt className="sr-only">Name</dt>
                         <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
                         <dt className="sr-only">Twitter</dt>
